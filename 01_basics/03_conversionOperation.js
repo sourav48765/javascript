20d1f678197516fc6fd50a52e5e62cfe03057331
Sourav Bisht <128303515+sourav48765@users.noreply.github.com>
2023-09-30 15:47:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "gdg"
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+console.log(booleanISLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sourav" => true
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
