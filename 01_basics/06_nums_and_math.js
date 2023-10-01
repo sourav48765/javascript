308a2c6f598ca800150e0d348652123526f782f9
Sourav Bisht <128303515+sourav48765@users.noreply.github.com>
2023-10-01 11:01:37 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const otherNumber = 11235.6464
+// console.log((otherNumber.toPrecision(4)));
+
+const Hundred = 1000000000
+// console.log(Hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++Math ++++++++++++++++++++
+
+// console.log(Math.random());
+// console.log(Math.ceil(5.21))
+// console.log(Math.min(4,5,7,1));
+// console.log(Math.max(4,5,7,1));
+const a = Math.random()
+console.log(a);
+// console.log(Math.ceil(a));
+// console.log(Math.floor(a)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
